e4ab929b8f4c0df24910b354176f99e73ab8f504
RakhaGalih <54633534+RakhaGalih@users.noreply.github.com>
2023-08-27 15:50:20 +0700

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,49 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    await Geolocator.requestPermission();
+    dynamic weatherData;
+    try {
+      weatherData = await Future.value(WeatherModel().getLocationWeather())
+          .timeout(const Duration(seconds: 5));
+    } catch (e) {
+      print('Failed to get data by $e');
+    }
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => LocationScreen(
+                  locationWeather: weatherData,
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: SpinKitDoubleBounce(
+        color: Colors.white,
+        size: 100.0,
+      ),
+    ));
+  }
+}
